5168f4f57bbd81b0a9c80aeb83723acc43959a3e
Jakaria425 <jakariahassan425@gmail.com>
2024-09-22 19:46:58 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'text-description': '#737373',
+      },
+    }
+  },
+  plugins: [],
+}
